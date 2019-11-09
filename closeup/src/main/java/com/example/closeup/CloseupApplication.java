b9e17cb033aa22e19038125bb1e280ddaf8fb14b
Hatice <melikeecevit@hotmail.com>
2019-11-09 15:55:13 +0300

    initialize the base project

---
+++ closeup/src/main/java/com/example/closeup/CloseupApplication.java
@@ -0,0 +1,13 @@
+package com.example.closeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloseupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloseupApplication.class, args);
+	}
+
+}
---
+++ closeup/src/test/java/com/example/closeup/CloseupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.closeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloseupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
